6067f6315a149889f241e35bf949de1717038532
Mihirgadhiya <gadhiyamihir257@gmail.com>
2024-07-10 16:50:30 +0530

    bootstreap-3

---
+++ bootstreap-3/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Template from './Template';
+
+function App() {
+  return (
+    <Template/>
+    
+  );
+}
+
+export default App;
---
+++ bootstreap-3/src/Template.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from './Compononts/Navbar'
+import Section1 from './Compononts/Section1'
+import Section2 from './Compononts/Section2'
+import Section3 from './Compononts/Section3'
+import Section4 from './Compononts/Section4'
+import Section5 from './Compononts/Section5'
+import Section6 from './Compononts/Section6'
+import Section7 from './Compononts/Section7'
+import Section8 from './Compononts/Section8'
+import Section9 from './Compononts/Section9'
+import Section10 from './Compononts/Section10'
+
+function Template() {
+  return (
+    <div>
+        <Navbar/>
+        <Section1/>
+        <Section2/>
+        <Section3/>
+        <Section4/>
+        <Section5/>
+        <Section6/>
+        <Section7/>
+        <Section8/>
+        <Section9/>
+        <Section10/>
+    </div>
+  )
+}
+
+export default Template
