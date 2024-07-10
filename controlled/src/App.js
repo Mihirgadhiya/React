baa981f287e87134bbfa2292363ac7e81121f2d8
Mihirgadhiya <gadhiyamihir257@gmail.com>
2024-07-10 16:52:19 +0530

    add

---
+++ controlled/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Cont from './Cont';
+import Uncont from './Uncont';
+
+function App() {
+  return (
+    <>
+    <Cont/>
+    <Uncont/>
+    </>
+  );
+}
+
+export default App;
